24d845e6e93b56f352b4ed74c0eb3ad13c71ac8e
juan <crumbled23@gmail.com>
2024-09-29 22:28:43 +0900

    ktable

--- src/main/java/com/example/springkafkapract/service/StreamService.java
+++ src/main/java/com/example/springkafkapract/service/StreamService.java
@@ -18,37 +18,37 @@ public class StreamService {
     @Autowired
     public void buildPipeline(StreamsBuilder sb) {
 
-//        //test topic에 aaa를 포함하는 msg가 들어오묜 TargetTopic으로 필터링
-//        KStream<String, String> stream =  sb.stream("test", Consumed.with(STRING_SERDE, STRING_SERDE));
-//        stream.filter((key, value) -> value.contains("aaa")).to("TargetTopic");
-
-
-        //join : key를 기준으로 ex) 1:leftvalue
-        KStream<String, String> left =  sb.stream("leftTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
-
-        KStream<String, String> right =  sb.stream("rightTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
-
-        ValueJoiner<String, String, String> stringJoiner = (leftValue, rightValue) -> {
-            return "[StringJoiner] " + leftValue + "-" + rightValue;
-        };
-
-        ValueJoiner<String, String, String> stringOuterJoiner = (leftValue, rightValue) -> {
-            return "[stringOuterJoiner] " + leftValue + "<" + rightValue;
-        };
-
-        KStream<String, String> joinStream = left.join(
-                right,
-                stringJoiner,
-                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
-
-        KStream<String, String> outerJoinStream = left.join(
-                right,
-                stringOuterJoiner,
-                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
-
-        joinStream.print(Printed.toSysOut());
-        joinStream.to("joinedMsg");
-        outerJoinStream.to("joinedMsg");
+////        //test topic에 aaa를 포함하는 msg가 들어오묜 TargetTopic으로 필터링
+////        KStream<String, String> stream =  sb.stream("test", Consumed.with(STRING_SERDE, STRING_SERDE));
+////        stream.filter((key, value) -> value.contains("aaa")).to("TargetTopic");
+//
+//
+//        //join : key를 기준으로 ex) 1:leftvalue
+//        KStream<String, String> left =  sb.stream("leftTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
+//
+//        KStream<String, String> right =  sb.stream("rightTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
+//
+//        ValueJoiner<String, String, String> stringJoiner = (leftValue, rightValue) -> {
+//            return "[StringJoiner] " + leftValue + "-" + rightValue;
+//        };
+//
+//        ValueJoiner<String, String, String> stringOuterJoiner = (leftValue, rightValue) -> {
+//            return "[stringOuterJoiner] " + leftValue + "<" + rightValue;
+//        };
+//
+//        KStream<String, String> joinStream = left.join(
+//                right,
+//                stringJoiner,
+//                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
+//
+//        KStream<String, String> outerJoinStream = left.join(
+//                right,
+//                stringOuterJoiner,
+//                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
+//
+//        joinStream.print(Printed.toSysOut());
+//        joinStream.to("joinedMsg");
+//        outerJoinStream.to("joinedMsg");
 
     }
 }
